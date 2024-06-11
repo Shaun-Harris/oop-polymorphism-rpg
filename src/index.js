81e7dbe137802408007a37570c89ac8bc68b5647
Shaun Harris <shaun@shaunwilliamharris.com>
2024-06-11 12:03:25 +0100

    domain model added, chracter class set up, fail test to ensure points are tallying as expected

---
+++ src/index.js
@@ -0,0 +1,17 @@
+class Character {
+    constructor(name, maxHitPoints, damage) {
+        this.name = name
+        this.maxHitPoints= maxHitPoints
+        this.currentHitPoints = maxHitPoints
+        this.damage = damage
+    }
+
+    takeDamage(attacker) {
+        this.currentHitPoints -= attacker.damage
+        if(this.currentHitPoints < 0) {
+            this.currentHitPoints = 0 
+        }
+    }
+}
+
+export { Character }
