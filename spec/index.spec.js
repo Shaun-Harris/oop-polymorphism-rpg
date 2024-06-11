203f051b7b8dcd324daee86804990b4c6a87c543
Shaun Harris <shaun@shaunwilliamharris.com>
2024-06-11 12:06:05 +0100

    domain model added, chracter class set up, passing test to ensure points are tallying as expected

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -3,7 +3,7 @@ import { Character } from "../src/index.js"
 //Test for Character Class 
 it('Character should take damage correctly', () => {
     const char1 = new Character('Char1', 100, 10)
-    const char2 = new Character('Char2', 100, 25)
+    const char2 = new Character('Char2', 100, 45)
 
     char1.takeDamage(char2)
     expect(char1.currentHitPoints).toBe(75)
